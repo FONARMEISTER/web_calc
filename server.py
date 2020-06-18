25f834142a3d944ba815c7483a7a367babcde40a
khaser <a-horohorin@mail.ru>
2020-06-18 15:43:27 +0300

    backend first commit

--- server.py
+++ server.py
@@ -31,16 +31,141 @@ def icons():
 
 @application.route('/', methods = ['POST'])
 def get_post_javascript_data():
-    # jsdata = request.form['params']
-    print(request.form)
+    log.info(request.form)
     if (request.form['action'] == 'get_results'):
         sess = random.randint(1, 10**18);
-        return "<h3> Итоговая стоимость заказа: " + 'TODO' + "</h3>" + "<div class='center' data-order='%s'> <span class='btn order'> Oформить заявку </span> </div>" % sess
+        return "<h3> Итоговая стоимость заказа: " + calc_cost(parse(request.form)) + "</h3>" + "<div class='center' data-order='%s'> <span class='btn order'> Oформить заявку </span> </div>" % sess
     else:
         sess = request.form['uorder']
-        print(sess)
         return "kek"
 
+def parse(jsdata):
+    def getMaterial(s):
+        if s == 'ЛДСП16':
+            return 16
+        elif s == 'ЛДСП26':
+            return 26
+        else:
+            return 4
+
+    def getName(s):
+        name = s[10:-1]
+        return name
+
+    def getRadius(s):
+        if s[-1] == '0':
+            return float(s[-2:])
+        else:
+            return float(s[0:2])
+    if jsdata['params[material]'] == '':
+        return "Введены не все данные, или данные не корректны"
+    d = {'material' : getMaterial(jsdata['params[material]']), 'details' : []}
+    for i in jsdata:
+        if 'detail' not in i:
+            continue
+        if 'type' in i:
+            d['details'].append({})
+            continue
+        ind = int(i[7]) // 4 - 1
+        if jsdata[i] == '0' or jsdata[i] == '':
+            return "Введены не все данные, или данные не корректны"
+        if getName(i)[0] == 'a':
+            d['details'][ind][getName(i)] = getRadius(jsdata[i])
+        else:
+            d['details'][ind][getName(i)] = float(jsdata[i])
+    return d
+
+def calc_cost(d):
+    material = d['material']
+    a = d['details']
+    s, p04, p2, paz1, paz2, R, Rless, Rmore, R1, R2 = 0
+    u, v, w, x = [0 for i in range len(a)] 
+    int cnt = -1
+    for i in a:
+        cnt += 1
+        s += (i['width'] * i['length'] * i['cnt'] / 10 ** 6)
+        p04 += (((i['lengthtop'] == 1) + (i['lengthbottom'] == 1)) * i['length'] + ((i['wighttop'] == 1) + (i['wightbottom'] == 1)) * i['wight']) * i['cnt'] / 1000
+        p2 += (((i['lengthtop'] == 2) + (i['lengthbottom'] == 2)) * i['length'] + ((i['wighttop'] == 2) + (i['wightbottom'] == 2)) * i['wight']) * i['cnt'] / 1000
+        paz1 += (i['paz'] == 1) * i['cnt']
+        pas2 += (i['paz'] == 2) * i['cnt']
+        if (i['r1'] > 0 and i['r4'] == 0):
+            u[cnt] += (i['length'] + i['width'])
+        else:
+            if (i['r1'] > 0 and i['r4'] > 0):
+                u[cnt] += (i['length'])
+        
+        if (i['r2'] > 0 and i['r1'] == 0):
+            v[cnt] += (i['length'] + i['width'])
+        else:
+            if (i['r2'] > 0 and i['r1'] > 0):
+                v[cnt] += (i['width'])
+
+        if (i['r3'] > 0 and i['r2'] == 0):
+            w[cnt] += (i['length'] + i['width'])
+        else:
+            if (i['r3'] > 0 and i['r2'] > 0):
+                w[cnt] += (i['length'])
+
+        if (i['r4'] > 0 and i['r3'] == 0):
+            x[cnt] += (i['length'] + i['width'])
+        else:
+            if (i['r4'] > 0 and i['r3'] > 0):
+                x[cnt] += (i['width'])
+        r = u[cnt]] + v[cnt] + w[cnt] + x[cnt]
+        R += r * i['cnt'] / 1000
+
+        for i in range(1, 5):
+            if (inRange(i['r' + str(i)], 1, 249)):
+	        Rless += i['cnt']
+        for i in range(1, 5):
+            if (i['r' + str(i)] >= 250):
+                Rmore += i['cnt']
+
+        if ((u[cnt] or v[cnt]) and i['lengthtop'] == 1)
+            R1 += i['length']
+        if ((w[cnt] or x[cnt]) and i['lengthbottom'] == 1)
+            R1 += i['length']
+        if ((v[cnt] or w[cnt]) and i['wighttop'] == 1)
+            R1 += i['width']
+        if ((x[cnt] or u[cnt]) and i['wightbottom'] == 1)
+            R1 += i['wight']
+
+        if ((u[cnt] or v[cnt]) and i['lengthtop'] == 2)
+            R2 += i['length']
+        if ((w[cnt] or x[cnt]) and i['lengthbottom'] == 2)
+            R2 += i['length']
+        if ((v[cnt] or w[cnt]) and i['wighttop'] == 2)
+            R2 += i['width']
+        if ((x[cnt] or u[cnt]) and i['wightbottom'] == 2)
+            R2 += i['wight']
+
+    R1 /= 1000
+    R2 /= 1000
+
+    res = 0
+    if (material == 16):
+        res += S * 370 * 1 + 70 * S
+    if (material == 26):
+        res += S * 450 * 1 + 90 * S
+    if (material == 4):
+        res += S * 150 * 1 + 25 * S
+
+    res += Rless * 120
+    res += Rmore * 250
+
+    if (material == 16):
+        res += (p04 - R1) * 33 + (p2 - R2) * 66
+    if (material == 26):
+        res += (p04 - R1) * 55 + (p2 - R2) * 150
+
+    if (material == 16):
+        res += R1 * 59 + R2 * 101
+    if (material == 26):
+        res += R1 * 109 + R2 * 296
+
+    res += paz1 * 20 + paz2 * 40
+    res += ceil(S / 5) * 120
+    return res
 
 
 def main():
